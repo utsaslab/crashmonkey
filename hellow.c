2528feb5ed7d833f54c1202fc747369492d5485b
ashmrtn <ashmrtn@carl.defaultdomain>
2016-11-26 17:24:31 +0000

    Get passthrough device to properly mount. Need to start pulling data from bios.

--- hellow.c
+++ hellow.c
@@ -5,10 +5,12 @@
 #include <linux/module.h>
 
 #define KERNEL_SECTOR_SIZE 512
+#define TARGET_DEVICE_PATH "/dev/vda"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ashmrtn");
 MODULE_DESCRIPTION("test hello world");
+
 
 static int major_num = 0;
 //module_param(major_num, int, 0);
@@ -22,6 +24,7 @@ static struct hwm_device {
   spinlock_t lock;
   u8* data;
   struct gendisk* gd;
+  struct block_device* target_dev;
 } Device;
 
 // The device operations structure.
@@ -31,8 +34,14 @@ static const struct block_device_operations hellow_ops = {
 };
 
 static void hellow_bio(struct request_queue* q, struct bio* bio) {
-  printk(KERN_INFO "hwm: Ending bio request\n");
-  bio_endio(bio, 0);
+  struct hwm_device* hwm;
+  struct request_queue* target_queue;
+
+  printk(KERN_INFO "hwm: passing request to normal block device driver\n");
+  hwm = (struct hwm_device*) q->queuedata;
+  target_queue = hwm->target_dev->bd_queue;
+  bio->bi_bdev = hwm->target_dev;
+  target_queue->make_request_fn(target_queue, bio);
 }
 
 static int __init hello_init(void) {
@@ -42,16 +51,18 @@ static int __init hello_init(void) {
   major_num = register_blkdev(major_num, "hwm");
   if (major_num <= 0) {
     printk(KERN_WARNING "hwm: unable to get major number\n");
-    goto out_unregister;
+    goto out;
+  }
+
+  Device.target_dev = blkdev_get_by_path(TARGET_DEVICE_PATH,
+      FMODE_READ | FMODE_WRITE, &Device);
+  if (!Device.target_dev) {
+    printk(KERN_WARNING "hwm: unable to grab underlying device\n");
+    goto out;
   }
 
   // Set up our internal device.
   spin_lock_init(&Device.lock);
-  Device.size = nsectors * hardsect_size;
-  Device.data = vmalloc(Device.size);
-  if (Device.data == NULL) {
-    return -ENOMEM;
-  }
 
   // And the gendisk structure.
   Device.gd = alloc_disk(1);
@@ -61,18 +72,19 @@ static int __init hello_init(void) {
 
   Device.gd->private_data = &Device;
   Device.gd->major = major_num;
-  Device.gd->first_minor = 0;
-  Device.gd->minors = 1;
-  set_capacity(Device.gd, nsectors * (hardsect_size / KERNEL_SECTOR_SIZE));
-  strcpy(Device.gd->disk_name, "hwm0");
+  Device.gd->first_minor = Device.target_dev->bd_disk->first_minor;
+  Device.gd->minors = Device.target_dev->bd_disk->minors;
+  set_capacity(Device.gd, get_capacity(Device.target_dev->bd_disk));
+  strcpy(Device.gd->disk_name, "hwm");
   Device.gd->fops = &hellow_ops;
 
   // Get a request queue.
-  Device.gd->queue = blk_alloc_queue_node(GFP_KERNEL, NUMA_NO_NODE);
+  Device.gd->queue = blk_alloc_queue(GFP_KERNEL);
   if (Device.gd->queue == NULL) {
     goto out;
   }
   blk_queue_make_request(Device.gd->queue, hellow_bio);
+  Device.gd->queue->queuedata = &Device;
   //blk_queue_hardsect_size(Queue, hardsect_size);
 
   add_disk(Device.gd);
@@ -81,13 +93,16 @@ static int __init hello_init(void) {
   return 0;
 
   out:
-    vfree(Device.data);
-  out_unregister:
     unregister_blkdev(major_num, "hwm");
     return -ENOMEM;
 }
 
 static void __exit hello_cleanup(void) {
+  blk_cleanup_queue(Device.gd->queue);
+  del_gendisk(Device.gd);
+  put_disk(Device.gd);
+  unregister_blkdev(major_num, "hwm");
+
   printk(KERN_INFO "hwm: Cleaning up\n");
 }
 
