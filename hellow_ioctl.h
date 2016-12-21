7c73373b2c3a3b22afa807ac64d468cdf5e0f570
ashmrtn <ashmrtn@utexas.edu>
2016-12-21 18:48:04 +0000

    Add macros for ioctl fetching of log data.

--- hellow_ioctl.h
+++ hellow_ioctl.h
@@ -1,7 +1,9 @@
 #ifndef HELLOW_IOCTL_H
 #define HELLOW_IOCTL_H
 
-#define HWM_LOG_OFF  0xff00
-#define HWM_LOG_ON   0xff01
+#define HWM_LOG_OFF           0xff00
+#define HWM_LOG_ON            0xff01
+#define HWM_GET_LOG_ENT_SIZE  0xff02
+#define HWM_GET_LOG_ENT       0xff03
 
 #endif
