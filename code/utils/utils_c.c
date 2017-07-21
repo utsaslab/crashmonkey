7bc04acc49a27ed6c625024a5dd3732cce14a34c
domingues <fabio.a.domingues@gmail.com>
2017-07-21 14:41:36 -0500

    Fix indentation.

--- code/utils/utils_c.c
+++ code/utils/utils_c.c
@@ -6,26 +6,26 @@
 #include <linux/blk_types.h>
 
 bool c_is_async_write(struct disk_write_op_meta *m) {
-    return !((m->bi_rw & REQ_SYNC) ||
-             (m->bi_rw & REQ_FUA) ||
-             (m->bi_rw & REQ_FLUSH) ||
-             (m->bi_rw & REQ_FLUSH_SEQ) ||
-             (m->bi_rw & REQ_SOFTBARRIER)) &&
-           (m->bi_rw & REQ_WRITE);
+  return !((m->bi_rw & REQ_SYNC) ||
+           (m->bi_rw & REQ_FUA) ||
+           (m->bi_rw & REQ_FLUSH) ||
+           (m->bi_rw & REQ_FLUSH_SEQ) ||
+           (m->bi_rw & REQ_SOFTBARRIER)) &&
+         (m->bi_rw & REQ_WRITE);
 }
 
 bool c_is_barrier_write(struct disk_write_op_meta *m) {
-    return ((m->bi_rw & REQ_FUA) ||
-            (m->bi_rw & REQ_FLUSH) ||
-            (m->bi_rw & REQ_FLUSH_SEQ) ||
-            (m->bi_rw & REQ_SOFTBARRIER)) &&
-           (m->bi_rw & REQ_WRITE);
+  return ((m->bi_rw & REQ_FUA) ||
+          (m->bi_rw & REQ_FLUSH) ||
+          (m->bi_rw & REQ_FLUSH_SEQ) ||
+          (m->bi_rw & REQ_SOFTBARRIER)) &&
+         (m->bi_rw & REQ_WRITE);
 }
 
 bool c_is_meta(struct disk_write_op_meta *m) {
-    return (m->bi_rw & REQ_META) && true;
+  return (m->bi_rw & REQ_META) && true;
 }
 
 bool c_has_write_flag(struct disk_write_op_meta *m) {
-    return (m->bi_rw & REQ_WRITE) && true;
+  return (m->bi_rw & REQ_WRITE) && true;
 }
