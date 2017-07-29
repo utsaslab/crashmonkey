0aa00c06885e50bac69e5cd2fb1286764f853085
Ashlie Martinez <ashmrtn@utexas.edu>
2017-07-29 01:27:25 +0000

    Move socket communication stuff over to a new, common directory. Up next is fixing code that relies on socket communication.

--- code/user_tools/begin_log.cpp
+++ code/user_tools/begin_log.cpp
@@ -1,5 +1,5 @@
-#include "communication.h"
-#include "helper.h"
+#include "../utils/communication/communication.h"
+#include "../utils/communication/helper.h"
 
 int main(int argc, char** argv) {
   return command_sender(SOCKET_NAME_OUTBOUND, HARNESS_BEGIN_LOG,
--- code/user_tools/helper.h
+++
@@ -1,5 +0,0 @@
-#include <cstdint>
-
-int command_sender(const char* path, int32_t command, int32_t reply);
-int send_command(int socket_fd, int32_t command);
-int32_t wait_for_command(int socket);
