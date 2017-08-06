00ec4efa4c7425a32c1bd214538de6614c1b838b
Ashlie Martinez <ashmrtn@utexas.edu>
2017-08-06 18:16:17 +0000

    Make the concept of "epochs" available at the Permuter level. Replaces implemetation that used to exist in the specific subclasses of Permuter.

--- code/permuter/RandomPermuter.cpp
+++ code/permuter/RandomPermuter.cpp
@@ -5,6 +5,7 @@
 
 #include <cassert>
 
+#include "Permuter.h"
 #include "RandomPermuter.h"
 
 namespace fs_testing {
@@ -18,157 +19,73 @@ using std::vector;
 
 using fs_testing::utils::disk_write;
 
-namespace {
-
-struct range {
-  unsigned int start;
-  unsigned int end;
-};
-
-}  // namespace
-
-void RandomPermuter::init_data_vector(vector<disk_write> *data) {
-  epochs.clear();
-  log_length = data->size();
-  unsigned int index = 0;
-  list<range> overlaps;
-  while (index < data->size()) {
-    struct epoch current_epoch;
-    current_epoch.has_barrier = false;
-    current_epoch.overlaps = false;
-    current_epoch.length = 0;
-
-    // Get all ops in this epoch and add them to either the sync_op or async_op
-    // lists.
-    while (index < data->size() && !(data->at(index)).is_barrier_write()) {
-      disk_write curr = data->at(index);
-      // Find overlapping ranges.
-      unsigned int start = curr.metadata.write_sector;
-      unsigned int end = start + curr.metadata.size;
-      for (auto range_iter = overlaps.begin(); range_iter != overlaps.end();
-          range_iter++) {
-        range r = *range_iter;
-        if ((r.start <= start && r.end >= start)
-            || (r.start <= end && r.end >= end)) {
-          if (r.start > start) {
-            r.start = start;
-          }
-          if (r.end < end) {
-            r.end = end;
-          }
-          current_epoch.overlaps = true;
-          break;
-        } else if (r.start > end) {
-          // Since this is an ordered list, if the next spot is past where we're
-          // looking now then we won't find anything past here. We may as well
-          // insert an item here.
-          range new_range = {start, end};
-          overlaps.insert(range_iter, new_range);
-        }
-      }
-
-      ++current_epoch.length;
-      epoch_op curr_op = {index, data->at(index)};
-      current_epoch.ops.push_back(curr_op);
-      current_epoch.num_meta += data->at(index).is_meta();
-      ++index;
-    }
-
-    // Check is the op at the current index is a "barrier." If it is then add it
-    // to the special spot in the epoch, otherwise just push the current epoch
-    // onto the list and move to the next segment of the log.
-    if (index < data->size() && (data->at(index)).is_barrier_write()) {
-      ++current_epoch.length;
-      epoch_op curr_op = {index, data->at(index)};
-      current_epoch.ops.push_back(curr_op);
-      current_epoch.num_meta += data->at(index).is_meta();
-      current_epoch.has_barrier = true;
-      ++index;
-    }
-    epochs.push_back(current_epoch);
-  }
-}
-
 RandomPermuter::RandomPermuter() {
-  log_length = 0;
   rand = mt19937(42);
 }
 
 RandomPermuter::RandomPermuter(vector<disk_write> *data) {
-  init_data_vector(data);
   // TODO(ashmrtn): Make a flag to make it random or not.
   rand = mt19937(42);
 }
 
-void RandomPermuter::init_data(vector<disk_write> *data) {
-  init_data_vector(data);
+void RandomPermuter::init_data(vector<epoch> *data) {
 }
 
-bool RandomPermuter::gen_one_state(vector<disk_write>& res) {
-  vector<unsigned int> permuted_epoch(0);
-  do {
-    unsigned int total_elements = 0;
-    // Find how many elements we will be returning (randomly determined).
-    uniform_int_distribution<unsigned int> permute_epochs(1, epochs.size());
-    unsigned int num_epochs = permute_epochs(rand);
-    // Don't subtract 1 from this size so that we can send a complete epoch if we
-    // want.
-    uniform_int_distribution<unsigned int> permute_requests(1,
-        epochs.at(num_epochs - 1).length);
-    unsigned int num_requests = permute_requests(rand);
-    for (unsigned int i = 0; i < num_epochs - 1; ++i) {
-      total_elements += epochs.at(i).length;
-    }
-    total_elements += num_requests;
-    res.resize(total_elements);
-    // Add all of the elements as some of the epochs could have been reordered
-    // if they overlapped.
-    permuted_epoch.resize(total_elements);
+bool RandomPermuter::gen_one_state(vector<epoch_op>& res) {
+  unsigned int total_elements = 0;
+  // Find how many elements we will be returning (randomly determined).
+  uniform_int_distribution<unsigned int> permute_epochs(1, GetEpochs()->size());
+  unsigned int num_epochs = permute_epochs(rand);
+  // Don't subtract 1 from this size so that we can send a complete epoch if we
+  // want.
+  uniform_int_distribution<unsigned int> permute_requests(1,
+      GetEpochs()->at(num_epochs - 1).ops.size());
+  unsigned int num_requests = permute_requests(rand);
+  for (unsigned int i = 0; i < num_epochs - 1; ++i) {
+    total_elements += GetEpochs()->at(i).ops.size();
+  }
+  total_elements += num_requests;
+  res.resize(total_elements);
 
-    auto curr_iter = res.begin();
-    auto permute_iter = permuted_epoch.begin();
-    for (unsigned int i = 0; i < num_epochs; ++i) {
-      if (epochs.at(i).overlaps || i == num_epochs - 1) {
-        unsigned int size =
-          (i != num_epochs - 1) ? epochs.at(i).length : num_requests;
-        auto res_end = curr_iter + size;
-        permute_epoch(curr_iter, res_end, permute_iter, epochs.at(i));
+  auto curr_iter = res.begin();
+  for (unsigned int i = 0; i < num_epochs; ++i) {
+    if (GetEpochs()->at(i).overlaps || i == num_epochs - 1) {
+      unsigned int size =
+        (i != num_epochs - 1) ? GetEpochs()->at(i).ops.size() : num_requests;
+      auto res_end = curr_iter + size;
+      permute_epoch(curr_iter, res_end, GetEpochs()->at(i));
 
-        curr_iter = res_end;
-        advance(permute_iter, size);
-      } else {
-        // Use a for loop since vector::insert inserts new elements and we
-        // resized above to the exact size we will have.
-        // We will only ever be placing the full epoch here because the above if
-        // will catch the case where we place only part of an epoch.
-        for (auto epoch_iter = epochs.at(i).ops.begin();
-            epoch_iter != epochs.at(i).ops.end(); ++epoch_iter) {
-          *curr_iter = epoch_iter->op;
-          ++curr_iter;
-          *permute_iter = epoch_iter->abs_index;
-          ++permute_iter;
-        }
+      curr_iter = res_end;
+    } else {
+      // Use a for loop since vector::insert inserts new elements and we
+      // resized above to the exact size we will have.
+      // We will only ever be placing the full epoch here because the above if
+      // will catch the case where we place only part of an epoch.
+      for (auto epoch_iter = GetEpochs()->at(i).ops.begin();
+          epoch_iter != GetEpochs()->at(i).ops.end(); ++epoch_iter) {
+        *curr_iter = *epoch_iter;
+        ++curr_iter;
       }
     }
-  } while (completed_permutations.count(permuted_epoch));
-  completed_permutations.insert(permuted_epoch);
-
+  }
   return true;
 }
 
 void RandomPermuter::permute_epoch(
-      vector<disk_write>::iterator& res_start,
-      vector<disk_write>::iterator& res_end,
-      vector<unsigned int>::iterator& order_start,
+      vector<epoch_op>::iterator& res_start,
+      vector<epoch_op>::iterator& res_end,
       epoch& epoch) {
-  assert(distance(res_start, res_end) <= epoch.length);
+  assert(distance(res_start, res_end) <= epoch.ops.size());
 
   // Even if the number of bios we're placing is less than the number in the
   // epoch, allow any bio but the barrier (if present) to be picked.
-  unsigned int slots = (epoch.has_barrier) ? epoch.length - 1 : epoch.length;
+  unsigned int slots = epoch.ops.size();
+  if (epoch.has_barrier) {
+    --slots;
+  }
 
-  // Fill the list with the empty slots, either [0, epoch.length - 1] or
-  // [0, epoch.length - 2]. Prefer a list so that removals are fast. We have
+  // Fill the list with the empty slots, either [0, epoch.size() - 1] or
+  // [0, epoch.size() - 2]. Prefer a list so that removals are fast. We have
   // this so that each time we pick a number we can find a bio which we haven't
   // already placed.
   list<unsigned int> empty_slots(slots);
@@ -182,10 +99,8 @@ void RandomPermuter::permute_epoch(
     uniform_int_distribution<unsigned int> uid(0, empty_slots.size() - 1);
     auto shift = empty_slots.begin();
     advance(shift, uid(rand));
-    *res_start = epoch.ops.at(*shift).op;
+    *res_start = epoch.ops.at(*shift);
     ++res_start;
-    *order_start = epoch.ops.at(*shift).abs_index;
-    ++order_start;
     empty_slots.erase(shift);
   }
 
@@ -199,8 +114,7 @@ void RandomPermuter::permute_epoch(
   // Place the barrier operation if it exists since the entire vector already
   // exists (i.e. we won't cause extra shifting when adding the other elements).
   // Decrement out count of empty slots since we have filled one.
-  *res_start = epoch.ops.back().op;
-  *order_start = epoch.ops.back().abs_index;
+  *res_start = epoch.ops.back();
 }
 
 }  // namespace permuter
--- code/permuter/RandomPermuter.h
+++ code/permuter/RandomPermuter.h
@@ -11,63 +11,19 @@
 namespace fs_testing {
 namespace permuter {
 
-struct BioVectorHash {
-  std::size_t operator() (const std::vector<unsigned int>& permutation) const {
-    unsigned int seed = permutation.size();
-    for (const auto& bio_pos : permutation) {
-      seed ^= bio_pos + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-    }
-    return seed;
-  }
-};
-
-struct BioVectorEqual {
-  bool operator() (const std::vector<unsigned int>& a,
-      const std::vector<unsigned int>& b) const {
-    if (a.size() != b.size()) {
-      return false;
-    }
-    for (unsigned int i = 0; i < a.size(); ++i) {
-      if (a.at(i) != b.at(i)) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-struct epoch_op {
-  unsigned int abs_index;
-  fs_testing::utils::disk_write op;
-};
-
-struct epoch {
-  unsigned int length;
-  unsigned int num_meta;
-  bool has_barrier;
-  bool overlaps;
-  std::vector<struct epoch_op> ops;
-};
-
 class RandomPermuter : public Permuter {
  public:
   RandomPermuter();
   RandomPermuter(std::vector<fs_testing::utils::disk_write> *data);
-  virtual void init_data(std::vector<fs_testing::utils::disk_write> *data);
-  virtual bool gen_one_state(std::vector<fs_testing::utils::disk_write>& res);
 
  private:
-  void init_data_vector(std::vector<fs_testing::utils::disk_write> *data);
+  virtual void init_data(std::vector<epoch> *data);
+  virtual bool gen_one_state(std::vector<epoch_op>& res);
   void permute_epoch(
-      std::vector<fs_testing::utils::disk_write>::iterator& res_start,
-      std::vector<fs_testing::utils::disk_write>::iterator& res_end,
-      std::vector<unsigned int>::iterator& order_start,
-      epoch& epoch);
-  unsigned int log_length;
-  std::vector<struct epoch> epochs;
+      std::vector<epoch_op>::iterator& res_start,
+      std::vector<epoch_op>::iterator& res_end, epoch& epoch);
+
   std::mt19937 rand;
-  std::unordered_set<std::vector<unsigned int>, BioVectorHash, BioVectorEqual>
-    completed_permutations;
 };
 
 }  // namespace permuter
