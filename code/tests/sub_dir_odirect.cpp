420ad9479622944d290bdc8c840757746fe99024
Ashlie Martinez <ashmrtn@utexas.edu>
2017-09-01 15:07:23 +0000

    Update tests to conform to new reporting system.

--- code/tests/sub_dir_odirect.cpp
+++ code/tests/sub_dir_odirect.cpp
@@ -9,9 +9,6 @@
 
 #include "BaseTestCase.h"
 
-using std::calloc;
-using std::string;
-
 #define TEST_FILE "test_file"
 #define TEST_DIR "test_dir"
 #define TEST_MNT "/mnt/snapshot"
@@ -27,7 +24,9 @@ using std::string;
 namespace fs_testing {
 namespace tests {
 
+using std::calloc;
 using std::memcmp;
+using std::string;
 
 class sub_dir_odirect : public BaseTestCase {
  public:
@@ -102,39 +101,44 @@ class sub_dir_odirect : public BaseTestCase {
     return 0;
   }
 
-  virtual int check_test() override {
-    int res2 = 0;
+  virtual int check_test(DataTestResult *test_result) override {
     for (unsigned int i = 0; i < NUM_TEST_FILES; ++i) {
       struct stat stats;
       const string file_name = string(TEST_MNT "/" TEST_DIR "/" TEST_FILE
           + std::to_string(i));
       int res = stat(file_name.c_str(), &stats);
       if (res < 0) {
-        return -1;
+        test_result->SetError(DataTestResult::kFileMissing);
+        return 0;
       }
       if (!S_ISREG(stats.st_mode)) {
-        return -1;
+        test_result->SetError(DataTestResult::kFileMetadataCorrupted);
+        return 0;
       }
       if (((S_IRWXU | S_IRWXG | S_IRWXO) & stats.st_mode) != TEST_FILE_PERMS) {
-        return -1;
+        test_result->SetError(DataTestResult::kFileMetadataCorrupted);
+        return 0;
       }
 
       const int fd = open(file_name.c_str(), O_RDONLY);
       if (fd < 0) {
-        return -1;
+        test_result->SetError(DataTestResult::kOther);
+        return 0;
       }
 
       unsigned int bytes_read = 0;
       char* buf = (char*) calloc(TEST_TEXT_SIZE, sizeof(char));
       if (buf == NULL) {
-        return -2;
+        test_result->SetError(DataTestResult::kOther);
+        return 0;
       }
       do {
         res = read(fd, buf + bytes_read, TEST_TEXT_SIZE - bytes_read);
         if (res < 0) {
           free(buf);
           close(fd);
-          return -1;
+          test_result->SetError(DataTestResult::kOther);
+          return 0;
         } else if (res == 0) {
           break;
         }
@@ -143,14 +147,14 @@ class sub_dir_odirect : public BaseTestCase {
       close(fd);
 
       if (bytes_read != TEST_TEXT_SIZE) {
-        res2 = -1;
+        test_result->SetError(DataTestResult::kFileDataCorrupted);
       } else if (memcmp(text, buf, TEST_TEXT_SIZE) != 0) {
-        res2 = -1;
+        test_result->SetError(DataTestResult::kFileDataCorrupted);
       }
 
       free(buf);
     }
-    return res2;
+    return 0;
   }
 
  private:
