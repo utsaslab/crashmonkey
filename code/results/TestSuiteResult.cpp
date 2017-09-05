6a1611e4b97e34f79e23e382a617c5be0c2c211f
Ashlie Martinez <ashmrtn@utexas.edu>
2017-09-05 13:24:07 -0500

    Mount file system prior to fsck for better fixes?

--- code/results/TestSuiteResult.cpp
+++ code/results/TestSuiteResult.cpp
@@ -25,6 +25,7 @@ void TestSuiteResult::PrintResults(ostream& os) const {
   unsigned int num_passed_fixed = 0;
   unsigned int num_passed = 0;
 
+  unsigned int fsck_required = 0;
   unsigned int old_file_persisted = 0;
   unsigned int file_missing = 0;
   unsigned int file_data_corrupted = 0;
@@ -38,6 +39,10 @@ void TestSuiteResult::PrintResults(ostream& os) const {
     } else if (result.fs_test.GetError() == FileSystemTestResult::kFixed
         && result.data_test.GetError() == DataTestResult::kClean) {
       ++num_passed_fixed;
+    } else if (result.fs_test.GetError() ==
+        FileSystemTestResult::kKernelMount &&
+        result.data_test.GetError() == DataTestResult::kClean) {
+      ++fsck_required;
     } else {
       ++num_failed;
       switch (result.data_test.GetError()) {
@@ -62,7 +67,9 @@ void TestSuiteResult::PrintResults(ostream& os) const {
 
   os << "Ran " << num_failed + num_passed_fixed + num_passed << " tests with"
     << "\n\tpassed cleanly: " << num_passed
-    << "\n\tpassed fixed: " << num_passed_fixed
+    << "\n\tpassed fixed: " << num_passed_fixed + fsck_required
+    << "\n\t\tfsck required: " << fsck_required
+    << "\n\t\tfsck fixed: " << num_passed_fixed
     << "\n\tfailed: " << num_failed
     << "\n\t\told file persisted: " << old_file_persisted
     << "\n\t\tfile missing: " << file_missing
