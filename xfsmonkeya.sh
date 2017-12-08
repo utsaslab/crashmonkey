e58b3d39f65970f48ca15302c8b19a5f67f1b453
gk159yo <mainali.subrat@utexas.edu>
2017-12-07 19:56:23 -0800

    summary feature

--- xfsmonkeya.sh
+++ xfsmonkeya.sh
@@ -1,9 +1,11 @@
-TEST_TYPE="ext4"
+TEST_TYPE="generic"
 FIRST_TEST=1
-LAST_TEST=5
+LAST_TEST=20
 SCRATCH_DEVICE="/dev/sda"
 SCRATCH_MOUNT="/mnt/sdbmount"
 OUTPUT_DIR="./output"
+
+SUMMARY_FILE='./summary'
 
 for((testNumber=$FIRST_TEST; testNumber<=$LAST_TEST; testNumber++))
 do
@@ -18,9 +20,18 @@ do
 	echo "About to run test case $TEST_TYPE/$paddedNumber"
 
 	sudo ./xfsmonkey.py --primary-dev TEST -t $SCRATCH_DEVICE $SCRATCH_MOUNT -e $TEST_TYPE/$paddedNumber > $TEST_CASE_DIR/out.txt 2>&1
-	sudo mv ./build/cm_out* $OUTPUT_DIR/$TEST_TYPE/$paddedNumber/
+	sudo mv ./build/cm_out* $TEST_CASE_DIR
 
 	echo "Ran test case $TEST_TYPE/$paddedNumber"
+
+	if [[ -z $(grep "passed fixed: 0" $TEST_CASE_DIR/out.txt) ]]
+	then
+		echo "PASSED FIXED: $TEST_CASE_DIR/out.txt" >> $SUMMARY_FILE
+	fi	
+	
+	if [[ -z $(grep "failed: 0" $TEST_CASE_DIR/out.txt) ]] 
+	then
+		echo "FAILED: $TEST_CASE_DIR/out.txt" >> $SUMMARY_FILE
+	fi
+
 done
-
-
