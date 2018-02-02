a51a06f2769de69bd2da47acd81407de0698e9b5
Ashlie Martinez <ashmrtn@utexas.edu>
2018-02-02 11:36:05 -0600

    Port cow_brd.c to 4.15 kernel. TODO: Test on previously tested kernel versions.

--- code/bio_alias.h
+++ code/bio_alias.h
@@ -10,19 +10,42 @@
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3, 13, 0) \
   && LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0)
 
+#define BI_RW                   bi_rw
+#define BI_DISK                 bi_bdev->bi_disk
 #define BI_SIZE                 bi_size
 #define BI_SECTOR               bi_sector
 #define BIO_ENDIO(bio, err)     bio_endio(bio, err)
+#define BIO_IO_ERR(bio, err)    bio_endio(bio, err)
+#define BIO_DISCARD_FLAG        REQ_DISCARD
+#define BIO_IS_WRITE(bio)       (!!(bio_rw(bio) & REQ_WRITE))
 
-#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 0) \
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 3, 0) \
   && LINUX_VERSION_CODE < KERNEL_VERSION(4, 5, 0)
 
+#define BI_RW                   bi_rw
+#define BI_DISK                 bi_bdev->bi_disk
 #define BI_SIZE                 bi_iter.bi_size
 #define BI_SECTOR               bi_iter.bi_sector
-#define BIO_ENDIO(bio, err)     bio->bi_error = err; bio_endio(bio)
+#define BIO_ENDIO(bio, err)     bio_endio(bio)
+#define BIO_IO_ERR(bio, err)    bio_io_error(bio)
+#define BIO_DISCARD_FLAG        REQ_DISCARD
+#define BIO_IS_WRITE(bio)       (!!(bio_rw(bio) & REQ_WRITE))
+
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(4, 15, 0) \
+  && LINUX_VERSION_CODE < KERNEL_VERSION(4, 16, 0)
+
+#define BI_RW                   bi_opf
+#define BI_DISK                 bi_disk
+#define BI_SIZE                 bi_iter.bi_size
+#define BI_SECTOR               bi_iter.bi_sector
+#define BIO_ENDIO(bio, err)     bio_endio(bio)
+#define BIO_IO_ERR(bio, err)    bio_io_error(bio)
+#define BIO_DISCARD_FLAG        REQ_OP_DISCARD
+#define BIO_IS_WRITE(bio)       op_is_write(bio_op(bio))
 
 #else
-#error "Unsupported kernel version: crashmonkey has not been tested with your kernel version."
+#error "Unsupported kernel version: CrashMonkey has not been tested with " \
+  "your kernel version."
 #endif
 
 #endif //CRASHMONKEY_BIO_ALIAS_H
