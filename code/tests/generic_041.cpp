47bd1c17ce1ae10646af833b3bd90fe3bce69b9f
jayashree <jaya@cs.utexas.edu>
2018-02-04 22:07:05 -0600

    btrfs bug that renders FS unmountable

--- code/tests/generic_041.cpp
+++ code/tests/generic_041.cpp
@@ -1,26 +1,30 @@
 /*
-Reproducing fstest generic/039
+Reproducing fstest generic/041
 
-1. Create directory A under TEST_MNT
-2. Create dir B under dir A
-3. Create file foo in dir B
-4. Create a link bar to file foo in the same dir B
-5. Sync the changes
-6. Remove the link bar to file foo
+1. Create file foo
+2. Create 3000 links for file foo in the same dir
+    (link_0 to link_2999)
+3. sync() everything so far
+4. Remove a link -> link_0
+5. Create a new link with a new name -> link_3000
+6. Create a new link with the old name -> link_0
 5. fsync(foo)
 
 If a power fail occurs now, and remount the filesystem,
-the dir B must be removable, after file foo is deleted.
+the dir A must be removable, after file foo and all links
+are deleted.
 
 
-This is tested to fail on btrfs(kernel 3.13) 
-On replaying the log tree, the i_size due to the removed link
-bar was not decremented in dir B, and left dangling directory
-index references. This makes B unremovable. This is also detected
-by btrfs check - returns error.
+This is tested to fail on btrfs(kernel 3.13) What happens here
+is that when we create a large number of links, some
+of which are extref and turn a regular ref into an 
+extref, fsync the inode and then replay the fsync log 
+we can endup with an fsync log that makes the replay code always fail with -EOVERFLOW when processing
+the inode's references. So the FS won't mount unless
+you explicitly use btrfs-zero-log to delete the fsync log.
 
-https://patchwork.kernel.org/patch/5853491/
-https://patchwork.kernel.org/patch/5576901/
+https://patchwork.kernel.org/patch/5622341/
+https://www.spinics.net/lists/linux-btrfs/msg41157.html
 */
 
 
@@ -38,7 +42,7 @@ https://patchwork.kernel.org/patch/5576901/
 #include "../user_tools/api/workload.h"
 #include "../user_tools/api/actions.h"
 #define TEST_FILE_FOO "foo"
-#define TEST_FILE_BAR "bar"
+#define TEST_FILE_FOO_LINK "foo_link_"
 #define TEST_MNT "/mnt/snapshot"
 #define TEST_DIR_A "test_dir_a"
 
@@ -55,7 +59,7 @@ namespace fs_testing {
 namespace tests {
 
 
-class Generic039: public BaseTestCase {
+class Generic041: public BaseTestCase {
  public:
   virtual int setup() override {
 
@@ -64,31 +68,51 @@ class Generic039: public BaseTestCase {
     if (res < 0) {
       return -1;
     }
-    sync();
-    return 0;
-  }
 
-  virtual int run() override {
-  
     //Create file foo in TEST_DIR_A 
     const int fd_foo = open(foo_path.c_str(), O_RDWR | O_CREAT, TEST_FILE_PERMS);
     if (fd_foo < 0) {
       return -1;
     }
 
-    //create a link bar for file foo
-    if (link(foo_path.c_str(), bar_path.c_str()) < 0){
-      return -2;
+    //create 3000 hard links
+    for (int i = 0; i < 3000; i++ ){
+      string foo_link = foo_link_path + std::to_string(i);
+      //std::cout << foo_link << std::endl;
+      if(link(foo_path.c_str(), foo_link.c_str()) < 0){
+        return -2;
+      }
     }
 
-    //Sync everything
     sync();
+    close(fd_foo);
+    return 0;
+  }
 
-    //remove file bar
-    if(remove(bar_path.c_str()) < 0){
+  virtual int run() override {
+  
+    const int fd_foo = open(foo_path.c_str(), O_RDWR | O_CREAT, TEST_FILE_PERMS);
+    if (fd_foo < 0) {
       return -1;
     }
 
+    //Remove one link : foo_link_0
+    string foo_link_old = foo_link_path + "0";
+    if(remove(foo_link_old.c_str()) < 0)
+      return -1;
+
+    //create a new link for file foo
+    string foo_link_new = foo_link_path + "3000";
+    if (link(foo_path.c_str(), foo_link_new.c_str()) < 0){
+      return -2;
+    }
+
+    //next create another link with old name - deleted link
+    if (link(foo_path.c_str(), foo_link_old.c_str()) < 0){
+      return -3;
+    }
+
+    //fsync foo
     int res = fsync(fd_foo);
     if (res < 0){
       return -4;
@@ -111,7 +135,6 @@ class Generic039: public BaseTestCase {
     char mode[] = "0777";
     int i_mode = strtol(mode,0,8);
     chmod(foo_path.c_str(), i_mode);
-    chmod(bar_path.c_str(), i_mode);
 
     //Remove all files within  test_dir_a
     system("rm -f /mnt/snapshot/test_dir_a/*");
@@ -120,43 +143,19 @@ class Generic039: public BaseTestCase {
     const int rem = rmdir(TEST_MNT "/" TEST_DIR_A);
     const int err = errno;
 
-    //Do we need a FS check to detect inconsistency?
-    bool need_check = false;
 
     //If rmdir failed because the dir was not empty, it's a bug
     if(rem < 0 && err == ENOTEMPTY){
       test_result->SetError(DataTestResult::kFileMetadataCorrupted);
       test_result->error_description = " : Cannot remove dir even if empty";    
-      need_check = true;
     }
 
-    //We do a FS check :
-    // We know this is a btrfs specific bug, so we use btrfs check
-    if(need_check){
-        system("umount /mnt/snapshot");
-        string check_string;
-        char tmp[128];
-        string command("btrfs check /dev/cow_ram_snapshot1_0 2>&1");
-        FILE *pipe = popen(command.c_str(), "r");
-        while (!feof(pipe)) {
-        char *r = fgets(tmp, 128, pipe);
-        // NULL can be returned on error.
-          if (r != NULL) {
-            check_string += tmp;
-           }
-        }
-
-        int ret = pclose(pipe);
-        if(ret != 0){
-          test_result->error_description += " \nbtrfs check result : \n " + check_string;
-        }
-      }
     return 0;
   }
 
    private:
     const string foo_path = TEST_MNT "/" TEST_DIR_A "/" TEST_FILE_FOO;    
-    const string bar_path = TEST_MNT "/" TEST_DIR_A "/" TEST_FILE_BAR;
+    const string foo_link_path = TEST_MNT "/" TEST_DIR_A "/" TEST_FILE_FOO_LINK;
     
 };
 
@@ -164,7 +163,7 @@ class Generic039: public BaseTestCase {
 }  // namespace fs_testing
 
 extern "C" fs_testing::tests::BaseTestCase *test_case_get_instance() {
-  return new fs_testing::tests::Generic039;
+  return new fs_testing::tests::Generic041;
 }
 
 extern "C" void test_case_delete_instance(fs_testing::tests::BaseTestCase *tc) {
