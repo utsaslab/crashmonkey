29db472a159a275109c62232733865e3f89a39cf
Jayashree <jaya@cs.utexas.edu>
2018-04-15 05:19:40 -0500

    Change writedata function to work with <4K writes

--- code/user_tools/src/workload.cpp
+++ code/user_tools/src/workload.cpp
@@ -45,8 +45,8 @@ int WriteData(int fd, unsigned int offset, unsigned int size) {
   const unsigned int rounded_offset =
     (offset + (kTestDataSize - 1)) & (~(kTestDataSize - 1));
   // Round down size to 4k for number of full pages to write.
-  const unsigned int aligned_size =
-    (size - (rounded_offset - offset)) & ~(kTestDataSize - 1);
+  
+  const unsigned int aligned_size = (size > kTestDataSize) ? (size - (rounded_offset - offset)) & ~(kTestDataSize - 1): 0;
   unsigned int num_written = 0;
 
   // The start of the write range is not aligned with our data blocks.
@@ -56,7 +56,7 @@ int WriteData(int fd, unsigned int offset, unsigned int size) {
   if (rounded_offset != offset) {
     // We should never write more than kTestDataSize of unaligned data at the
     // start.
-    do {
+    while (offset + num_written < rounded_offset){
       const unsigned int mod_offset =
         num_written + (offset & (kTestDataSize - 1));
       assert(mod_offset < kTestDataSize);
@@ -67,13 +67,13 @@ int WriteData(int fd, unsigned int offset, unsigned int size) {
         return errno;
       }
       num_written += res;
-    } while (offset + num_written < rounded_offset);
+    }
   }
 
   // Write out the required number of full pages for this request. The first
   // byte will be aligned with kTestDataSize.
   unsigned int aligned_written = 0;
-  do {
+  while (aligned_written < aligned_size) {
     const unsigned int mod_offset = (aligned_written & (kTestDataSize - 1));
     // Write up to a full page of data at a time.
     int res = pwrite(fd, kTestDataBlock + mod_offset,
@@ -83,7 +83,7 @@ int WriteData(int fd, unsigned int offset, unsigned int size) {
     }
     num_written += res;
     aligned_written += res;
-  } while (aligned_written < aligned_size);
+  } 
 
   if (num_written == size) {
     return 0;
@@ -92,7 +92,7 @@ int WriteData(int fd, unsigned int offset, unsigned int size) {
   // Write out the last partial page of data. The first byte will be aligned
   // with kTestDataSize.
   unsigned int end_written = 0;
-  do {
+  while (num_written < size) {
     assert(end_written < kTestDataSize);
     const unsigned int mod_offset = (end_written & (kTestDataSize - 1));
     int res = pwrite(fd, kTestDataBlock + mod_offset,
@@ -102,7 +102,7 @@ int WriteData(int fd, unsigned int offset, unsigned int size) {
     }
     num_written += res;
     end_written += res;
-  } while (num_written < size);
+  } 
 
   return 0;
 }
