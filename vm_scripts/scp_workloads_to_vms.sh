ceaf196b0542cb81af927955f13f7459571d2d3c
Pandian <pandian4mail@gmail.com>
2018-04-15 21:58:57 -0500

    Removing seq2 directory initially in the remote VMs

--- vm_scripts/scp_workloads_to_vms.sh
+++ vm_scripts/scp_workloads_to_vms.sh
@@ -9,6 +9,7 @@ num_vms=$1
 
 port=3022
 for i in `seq 1 $num_vms`; do
+	rsh -p $port pandian@127.0.0.1 "rm -r ~/seq2"
 	rsh -p $port pandian@127.0.0.1 "mkdir -p ~/seq2"
 	scp -P $port ~/seq2/vm$i/* pandian@127.0.0.1:~/seq2/
 	scp -P $port ~/vm_remote_script.sh pandian@127.0.0.1:~/
