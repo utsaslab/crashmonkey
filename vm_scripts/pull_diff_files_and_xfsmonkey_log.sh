dde761f4925e2221e14bc5e12c759a513937b0e8
Pandian <pandian4mail@gmail.com>
2018-04-15 21:48:43 -0500

    Adding run id to the diff file pull script

--- vm_scripts/pull_diff_files_and_xfsmonkey_log.sh
+++ vm_scripts/pull_diff_files_and_xfsmonkey_log.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters; Please provide start server number and end server number as the parameters;"
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters; Please provide unique run identifier, start server number and end server number as the parameters;"
     exit 1
 fi
 
 i=1
-st=$1
-end=$2
+run=$1
+st=$2
+end=$3
 
-mkdir -p diff_files
-mkdir -p xfsmonkey_log
+mkdir -p $run
+mkdir -p $run/diff_files
+mkdir -p $run/xfsmonkey_logs
 
 for ip in `cat live_nodes`; do
         echo `date` ------------- Checking for diff file from node $i IP $ip -----------------
@@ -30,8 +32,8 @@ for ip in `cat live_nodes`; do
 
 	port=3022
 	for j in `seq 1 $num_vms`; do
-		sshpass -p "alohomora" scp -o "StrictHostKeyChecking no" -P $port pandian@$ip:~/projects/crashmonkey/diff_results/* diff_files/
-		sshpass -p "alohomora" scp -o "StrictHostKeyChecking no" -P $port pandian@$ip:~/projects/crashmonkey/xfsmonkey*.log xfsmonkey_log/
+		sshpass -p "alohomora" scp -o "StrictHostKeyChecking no" -P $port pandian@$ip:~/projects/crashmonkey/diff_results/* $run/diff_files/
+		sshpass -p "alohomora" scp -o "StrictHostKeyChecking no" -P $port pandian@$ip:~/projects/crashmonkey/xfsmonkey*.log $run/xfsmonkey_logs/
 		port=`expr $port + 1`
 	done
 	i=`expr $i + 1`
