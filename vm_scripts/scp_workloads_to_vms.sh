d64fe8cd1676a44adbfc270746a77fb2eb274bcc
Pandian <pandian4mail@gmail.com>
2018-04-15 22:21:55 -0500

    Bug fixes in the scripts

--- vm_scripts/scp_workloads_to_vms.sh
+++ vm_scripts/scp_workloads_to_vms.sh
@@ -11,8 +11,14 @@ port=3022
 for i in `seq 1 $num_vms`; do
 	rsh -p $port pandian@127.0.0.1 "rm -r ~/seq2"
 	rsh -p $port pandian@127.0.0.1 "mkdir -p ~/seq2"
+	
+	# Copy the workloads
 	scp -P $port ~/seq2/vm$i/* pandian@127.0.0.1:~/seq2/
-	scp -P $port ~/vm_remote_script.sh pandian@127.0.0.1:~/
+	
+	# Copy the other remote scripts
+	scp -P $port ~/vm_remote_trigger_script.sh pandian@127.0.0.1:~/
+	scp -P $port ~/vm_remote_check_running.sh pandian@127.0.0.1:~/
 	scp -P $port ~/cm_cleanup.sh pandian@127.0.0.1:~/
+	
 	port=`expr $port + 1`
 done
--- vm_scripts/trigger_remote_trigger_script.sh
+++ vm_scripts/trigger_remote_trigger_script.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters; Please provide number of vms as the parameter;"
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters; Please provide number of vms and file system as the parameters;"
     exit 1
 fi
 
 num_vms=$1
+fs=$2
 
 port=3022
 for i in `seq 1 $num_vms`; do
-	rsh -p $port pandian@127.0.0.1 "echo alohomora| sudo -S bash ~/vm_remote_trigger_script.sh"
+	rsh -p $port pandian@127.0.0.1 "echo alohomora| sudo -S bash /home/pandian/vm_remote_trigger_script.sh "$fs
 	port=`expr $port + 1`
 done
