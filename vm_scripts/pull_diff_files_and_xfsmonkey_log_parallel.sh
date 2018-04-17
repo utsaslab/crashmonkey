95df965a4dc051c4d0c2382a3e3a7c1bba3c5e76
Pandian <pandian4mail@gmail.com>
2018-04-17 01:08:21 -0500

    Bug fix in parallel scripts

--- vm_scripts/pull_diff_files_and_xfsmonkey_log_parallel.sh
+++ vm_scripts/pull_diff_files_and_xfsmonkey_log_parallel.sh
@@ -12,5 +12,5 @@ run=$1
 for i in `seq 1 10`; do
 	nohup ./pull_diff_files_and_xfsmonkey_log.sh $run $st $end > out$i.log &
 	st=`expr $st + 5`
-	end=`expr $st + 5`
+	end=`expr $end + 5`
 done
--- vm_scripts/scp_segregated_workloads_parallel.sh
+++ vm_scripts/scp_segregated_workloads_parallel.sh
@@ -6,5 +6,5 @@ end=5
 for i in `seq 1 10`; do
 	nohup ./scp_segregated_workloads.sh $st $end > out$i.log &
 	st=`expr $st + 5`
-	end=`expr $st + 5`
+	end=`expr $end + 5`
 done
