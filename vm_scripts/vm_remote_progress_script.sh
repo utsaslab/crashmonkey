67e8db9fa50cbe6888d30176c2e9cb24fb3dd4ce
Pandian <pandian4mail@gmail.com>
2018-04-19 10:10:21 -0500

    Fixing typo in remote progress script

--- vm_scripts/vm_remote_progress_script.sh
+++ vm_scripts/vm_remote_progress_script.sh
@@ -2,4 +2,4 @@
 
 completed=`cat /home/pandian/projects/crashmonkey/xfsmonkey*.log | grep Test | tail -n1 | cut -d# -f2 | cut -d ' ' -f2`
 total=`ls -lah /home/pandian/projects/crashmonkey/build/xfsMonkeyTests/j-lang* | grep -v grep | wc -l`
-echo -------- `uname -n` '    ' $complete/$total completed.
+echo -------- `uname -n` '    ' $completed/$total completed.
