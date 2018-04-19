303dfeeaad3e0317f9b669eb25dc4c45d78d0ffa
Pandian <pandian4mail@gmail.com>
2018-04-18 22:22:41 -0500

    Adding -y flag to apt-get install in setup

--- vm_scripts/setup.sh
+++ vm_scripts/setup.sh
@@ -8,10 +8,10 @@ mkdir projects
 cd projects/
 
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git
 
 git clone https://github.com/utsaslab/crashmonkey
-sudo apt-get install btrfs-tools f2fs-tools xfsprogs libelf-dev
+sudo apt-get install -y btrfs-tools f2fs-tools xfsprogs libelf-dev
 
 cd ~/
 
@@ -23,10 +23,10 @@ wget https://download.virtualbox.org/virtualbox/5.2.8/virtualbox-5.2_5.2.8-12100
 wget https://download.virtualbox.org/virtualbox/5.2.8/Oracle_VM_VirtualBox_Extension_Pack-5.2.8-121009.vbox-extpack
 
 #sudo apt-get install linux-headers-generic linux-headers-4.4.0-116-generic
-sudo apt-get install dkms build-essential linux-headers-generic linux-headers-`uname -r`
+sudo apt-get install -y dkms build-essential linux-headers-generic linux-headers-`uname -r`
 
 sudo dpkg -i virtualbox*.deb
-sudo apt-get install -f
+sudo apt-get install -y -f
 
 #sudo apt install virtualbox*.deb
 sudo VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-5.2.8-121009.vbox-extpack
