14515087c652a7fa01ddb1645960b96329717868
Pandian <pandian4mail@gmail.com>
2018-04-19 01:32:54 -0500

    Skipping execution of xfsMonkey in remote trigger script if it's already running

--- vm_scripts/trigger_remote_script_trigger_xfsMonkey.sh
+++ vm_scripts/trigger_remote_script_trigger_xfsMonkey.sh
@@ -10,6 +10,8 @@ fs=$1
 
 port=3022
 for i in `seq 1 $num_vms`; do
+	echo Triggering xfsMonkey in VM $i...
 	rsh -p $port pandian@127.0.0.1 "echo alohomora| sudo -S bash /home/pandian/vm_remote_trigger_script.sh "$fs
+	sleep 1
 	port=`expr $port + 1`
 done
