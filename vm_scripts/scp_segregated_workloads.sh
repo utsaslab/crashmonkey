5b309350932c8c35b2005270ce2053b10fc291d1
Pandian <pandian4mail@gmail.com>
2018-04-24 16:40:30 -0500

    bug fix in scp_segregate script: making num_vms as 16

--- vm_scripts/scp_segregated_workloads.sh
+++ vm_scripts/scp_segregated_workloads.sh
@@ -9,17 +9,11 @@ i=1
 
 st=$1
 end=$2
+num_vms=16
 
 for ip in `cat live_nodes`; do
 	echo `date` ------------- SCPing segregated workloads to node $i IP $ip -----------------
 
-	if [ $i -le 6 ]
-	then
-		num_vms=8
-	else
-		num_vms=16
-	fi
-	
 	if [ $i -lt $st ] || [ $i -gt $end ]
 	then
 		i=`expr $i + 1`
