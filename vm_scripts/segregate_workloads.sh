9053c13c41d923378e3159ee3a125c594584d4c3
Pandian <pandian4mail@gmail.com>
2018-04-24 16:29:47 -0500

    Making segregate_workload script customizable

--- vm_scripts/segregate_workloads.sh
+++ vm_scripts/segregate_workloads.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters; Please provide num workloads per vm, max workload number and base path to workload files as the parameters;"
+    exit 1
+fi
+
 i=1
 k=1
-num_per_vm=87
-max=65000
+num_per_vm=$1
+max=$2
+workload_base_path=$3
+num_vms=$num_vms
 
 for ip in `cat live_nodes`; do
 	echo `date` ------------- Segregating to node $i IP $ip starting from k $k -----------------
 	mkdir -p workloads/seg/node"$i"-"$ip"
-
-	if [ $i -le 6 ]
-	then
-		num_vms=8
-	else
-		num_vms=16
-	fi
 
 	for j in `seq 1 $num_vms`; do
 		echo `date` Segregating to vm $j of node $i ...
         	mkdir -p workloads/seg/node"$i"-"$ip"/vm"$j"
 		
 		for l in `seq 1 $num_per_vm`; do
-			cp workloads/seq2/j-lang"$k".cpp workloads/seg/node"$i"-"$ip"/vm"$j"/
+			cp $workload_base_path/j-lang"$k".cpp workloads/seg/node"$i"-"$ip"/vm"$j"/
 			k=`expr $k + 1`
 			
 			if [ $k -gt $max ]
