8195f5b9b55f85fff59a05678558b02bb9cc3f53
Pandian <pandian4mail@gmail.com>
2018-04-25 14:31:25 -0500

    Adding num_diffs to progress script

--- vm_scripts/vm_remote_progress_script.sh
+++ vm_scripts/vm_remote_progress_script.sh
@@ -2,4 +2,5 @@
 
 completed=`cat /home/pandian/projects/crashmonkey/xfsmonkey*.log | grep Test | tail -n1 | cut -d# -f2 | cut -d ' ' -f2`
 total=`ls -lah /home/pandian/projects/crashmonkey/build/xfsMonkeyTests/j-lang* | grep -v grep | wc -l`
-echo -------- `uname -n` '    ' $completed/$total completed.
+num_diffs=`ls -lah /home/pandian/projects/crashmonkey/diff_results/j-lang* | grep -v grep | wc -l`
+echo ------ `uname -n` '  ' $completed/$total completed; $num_diffs diffs generated.
