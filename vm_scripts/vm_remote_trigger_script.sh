d480399e6ce24f158aa7ee569b581f3fa0f12d67
Pandian <pandian4mail@gmail.com>
2018-04-24 20:24:28 -0500

    Adding date to log of vm_remote_trigger_script

--- vm_scripts/vm_remote_trigger_script.sh
+++ vm_scripts/vm_remote_trigger_script.sh
@@ -11,23 +11,23 @@ num=`ps aux | grep xfsMonkey | grep -v grep | wc -l`
 
 if [ $num -ne 0 ]
 then
-        echo xfsMonkey is already running.. Skipping the triggering of xfsMonkey..  
+        echo `date` 'xfsMonkey is already running.. Skipping the triggering of xfsMonkey..  '
 	exit
 fi
 
 # install libattr for xattr headers
-echo Installing libattr1-dev..
+echo `date` 'Installing libattr1-dev..'
 echo alohomora | sudo -S apt-get install --yes libattr1-dev btrfs-tools f2fs-tools xfsprogs
 
 # update crashmonkey repo
-echo Updating crashmonkey repo..
+echo `date` 'Updating crashmonkey repo..'
 cd projects/crashmonkey
 
 echo alohomora | sudo -S git checkout .
 echo alohomora | sudo -S git pull
 echo alohomora | sudo -S git checkout testing_seq_1
 
-echo Removing log files..
+echo `date`  'Removing log files..'
 echo alohomora | sudo -S rm *.log
 echo alohomora | sudo -S rm build/*.log
 echo alohomora | sudo -S rm diff_results/*
@@ -35,25 +35,25 @@ echo alohomora | sudo -S rm diff_results/*
 # Do cm cleanup -> rm cow_brd, disk_wrapper, umount etc
 echo alohomora | sudo -S bash /home/pandian/cm_cleanup.sh
 
-echo Creating /mnt/snapshot..
+echo `date`  'Creating /mnt/snapshot..'
 echo alohomora | sudo -S mkdir -p /mnt/snapshot
 
 echo alohomora | sudo -S rm code/tests/j-lang*.cpp
 echo alohomora | sudo -S rm build/tests/j-lang*.so
 
-echo Copying cpp workload files to code/tests/..
+echo `date`  'Copying cpp workload files to code/tests/..'
 cp ~/seq2/* code/tests/
 
-echo Compiling..
+echo `date`  'Compiling..'
 make
 
-echo Moving built so files to xfsMonkeyTests/..
+echo `date`  'Moving built so files to xfsMonkeyTests/..'
 
 echo alohomora | sudo -S rm -r build/xfsMonkeyTests
 
 echo alohomora | sudo -S mkdir -p build/xfsMonkeyTests
 echo alohomora | sudo -S mv build/tests/j-lang*.so build/xfsMonkeyTests/
 
-echo 'Triggering xfsMonkey run..'
+echo `date` 'Triggering xfsMonkey run..'
 nohup echo alohomora | sudo -S python xfsMonkey.py -f /dev/sda -d /dev/cow_ram0 -t $fs -e 102400 > xfsmonkey_seq2_"$fs"_run1.log &
-echo 'Triggered.'
+echo `date`  'Triggered.'
