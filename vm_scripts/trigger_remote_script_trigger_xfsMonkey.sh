9c94642e2f17e9c17f38451e5b860488af431920
Pandian <pandian4mail@gmail.com>
2018-04-26 13:54:37 -0500

    Changing the remote trigger script to trigger the run in nohup

--- vm_scripts/trigger_remote_script_trigger_xfsMonkey.sh
+++ vm_scripts/trigger_remote_script_trigger_xfsMonkey.sh
@@ -11,7 +11,14 @@ fs=$1
 port=3022
 for i in `seq 1 $num_vms`; do
 	echo `date` 'Triggering xfsMonkey in VM '$i'...'
-	rsh -p $port pandian@127.0.0.1 "echo alohomora| sudo -S bash /home/pandian/vm_remote_trigger_script.sh "$fs
-	sleep 1
+	rsh -p $port pandian@127.0.0.1 "nohup echo alohomora| sudo -S bash /home/pandian/vm_remote_trigger_script.sh "$fs" &> trigger_"$fs".log &"
+	echo Sleeping for 10 seconds..
+	sleep 10
+
+	echo `date` ' Checking if trigger process is running in VM '$i'...'
+	rsh -p $port pandian@127.0.0.1 "ps aux | grep -e xfsMonkey -e vm_remote_trigger"
+	echo Sleeping for 5 seconds..
+	sleep 5
+
 	port=`expr $port + 1`
 done
