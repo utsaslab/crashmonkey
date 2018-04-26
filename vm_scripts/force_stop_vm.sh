d659ac27495a4b7d74807632fd06648326046546
Pandian <pandian4mail@gmail.com>
2018-04-26 00:17:56 -0500

    adding force stop vm for a single vm

---
+++ vm_scripts/force_stop_vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters; Please provide vm number as the parameter;"
+    exit 1
+fi
+
+vm=$1
+kill `ps aux | grep startvm"$vm" | grep -v grep | tr -s ' ' | cut -d ' ' -f2`
+
