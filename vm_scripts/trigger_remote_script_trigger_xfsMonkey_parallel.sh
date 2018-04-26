bfb9fc637be882d4f795688dd7915eb07f54554a
Pandian <pandian4mail@gmail.com>
2018-04-25 22:10:19 -0500

    Bug fix in trigger parallel script

--- vm_scripts/trigger_remote_script_trigger_xfsMonkey_parallel.sh
+++ vm_scripts/trigger_remote_script_trigger_xfsMonkey_parallel.sh
@@ -14,7 +14,7 @@ st=1
 end=`expr $st + $batch_size - 1`
 
 i=1
-while [ $st -le $num_servers ]; do
+while [ $st -le $num_vms ]; do
 	nohup ./trigger_remote_script_trigger_xfsMonkey_range.sh $fs $st $end > out_trigger_log_"$fs"_"$st"_"$end".log &
 	st=`expr $st + $batch_size`
 	end=`expr $end + $batch_size`
