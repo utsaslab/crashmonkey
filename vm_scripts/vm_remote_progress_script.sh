65906cf4bcd91c52c8a72de8da413914e8a1b6f8
Pandian <pandian4mail@gmail.com>
2018-04-27 11:50:33 -0500

    Adding count of RAM disk errors to progress script

--- vm_scripts/vm_remote_progress_script.sh
+++ vm_scripts/vm_remote_progress_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 completed=`cat /home/pandian/projects/crashmonkey/xfsmonkey*.log | grep Test | tail -n1 | cut -d# -f2 | cut -d ' ' -f2`
+ram_disk_error=`cat /home/pandian/projects/crashmonkey/xfsmonkey*.log | grep 'Error inserting RAM disk module' | wc -l`
 total=`ls -lah /home/pandian/projects/crashmonkey/build/xfsMonkeyTests/j-lang* | grep -v grep | wc -l`
 num_diffs=`ls -lah /home/pandian/projects/crashmonkey/diff_results/ | grep j-lang | grep -v grep | wc -l`
-echo ------ `uname -n` '   '$completed/$total' completed; '$num_diffs' diffs generated.'
+echo ------ `uname -n` '   '$completed/$total' completed; '$ram_disk_error' RAM disk errors! '$num_diffs' diffs generated.'
