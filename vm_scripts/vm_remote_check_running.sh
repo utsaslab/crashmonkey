69c10fd792892f6d00db09b27f904b5904793f21
Pandian <pandian4mail@gmail.com>
2018-04-27 15:15:30 -0500

    Adding hostname info to check_progress script

--- vm_scripts/vm_remote_check_running.sh
+++ vm_scripts/vm_remote_check_running.sh
@@ -4,7 +4,7 @@ num=`ps aux | grep xfsMonkey | grep -v grep | wc -l`
 
 if [ $num -eq 0 ]
 then
-	echo 'No runs in progress'
+	echo `uname -n` 'No runs in progress'
 else
-	echo 'Some run is in progress'
+	echo `uname -n` 'Some run is in progress'
 fi
