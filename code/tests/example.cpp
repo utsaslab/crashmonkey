aa42f8f39bd8d36efa1853fb8f35909f8b33e721
Jayashree <jaya@cs.utexas.edu>
2018-09-26 15:05:01 -0500

    Adding more details on test case setup and a sample test

---
+++ code/tests/example.cpp
@@ -0,0 +1,145 @@
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <dirent.h>
+#include <cstring>
+#include <errno.h>
+#include <attr/xattr.h>
+
+#include "BaseTestCase.h"
+#include "../user_tools/api/workload.h"
+#include "../user_tools/api/actions.h"
+
+using fs_testing::tests::DataTestResult;
+using fs_testing::user_tools::api::WriteData;
+using fs_testing::user_tools::api::WriteDataMmap;
+using fs_testing::user_tools::api::Checkpoint;
+using std::string;
+
+namespace fs_testing {
+    namespace tests {
+
+
+        class Example: public BaseTestCase {
+
+            public:
+
+              virtual int setup() override {
+
+                // initialize paths.
+                // mnt_dir_ points to the root directory by default
+                  A_path = mnt_dir_ + "/A";
+                  Afoo_path =  mnt_dir_ + "/A/foo";
+
+                  // create the directory
+                  if ( mkdir(A_path.c_str() , 0777) < 0 ){
+                      return errno;
+                  }
+
+                  // get the directory handle
+                  int fd_Afoo = cm_->CmOpen(Afoo_path.c_str() , O_RDWR|O_CREAT , 0777);
+                  if ( fd_Afoo < 0 ) {
+                    cm_->CmClose( fd_Afoo);
+                    return errno;
+                  }
+
+                  cm_->CmSync();
+
+                  //close the file decriptor.
+                  if ( cm_->CmClose ( fd_Afoo ) < 0 ) {
+                      return errno;
+                  }
+
+                  return 0;
+              }
+
+              virtual int run( int checkpoint ) override {
+
+                B_path = mnt_dir_ + "/B";
+                Bfoo_path =  mnt_dir_ + "/B/foo";
+                Afoo_path =  mnt_dir_ + "/A/foo";
+                int local_checkpoint = 0 ;
+
+                // create the directory
+                if ( mkdir( B_path.c_str() , 0777 ) < 0 ) {
+                    return errno;
+                }
+
+                if ( link( Afoo_path.c_str() , Bfoo_path.c_str() ) < 0 ) {
+                  return errno;
+                }
+
+                if ( cm_->CmFsync( fd_Afoo ) < 0 ) {
+                  return errno;
+                }
+
+                if ( cm_->CmCheckpoint() < 0){
+                    return -1;
+                }
+                local_checkpoint += 1;
+                if (local_checkpoint == checkpoint) {
+                    return 1;
+                }
+
+                if ( cm_->CmClose ( fd_Afoo) < 0){
+                    return errno;
+                }
+
+                return 0;
+            }
+
+            virtual int check_test( unsigned int last_checkpoint, DataTestResult *test_result) override {
+
+              Bfoo_path =  mnt_dir_ + "/B/foo";
+              Afoo_path =  mnt_dir_ + "/A/foo";
+
+              struct stat stats;
+
+              /*
+              Because we persisted A/foo in setup, and didn't rename or unlink
+              it, we must find A/foo after crash.
+              */
+              int res_Afoo = stat(Afoo_path.c_str(), &stats);
+              if (res_Afoo < 0) {
+                  test_result->SetError(DataTestResult::kFileMissing);
+                  test_result->error_description = " : Missing file " + Afoo_path.c_str();
+                  return 0;
+              }
+
+              /*
+              We need to check for the existance of B/foo only after A/foo has
+              been persisted, which is tracked by checkpoint.
+              */
+
+              int res_Bfoo = stat(Bfoo_path.c_str(), &stats);
+              if ( last_checkpoint == 1 && res_Bfoo < 0 ) {
+                  test_result->SetError(DataTestResult::kFileMissing);
+                  test_result->error_description = " : Missing file " + Bfoo_path.c_str();
+                  return 0;
+              }
+
+              return 0;
+            }
+
+          private:
+
+            string A_path;
+            string B_path;
+            string Afoo_path;
+            string Bfoo_path;
+
+      };
+
+  }  // namespace tests
+  }  // namespace fs_testing
+
+ extern "C" fs_testing::tests::BaseTestCase *test_case_get_instance() {
+     return new fs_testing::tests::Example;
+ }
+
+ extern "C" void test_case_delete_instance(fs_testing::tests::BaseTestCase *tc) {
+     delete tc;
+ }
--- documentation/workload.md
+++ documentation/workload.md
@@ -41,17 +41,118 @@ ___
 ### Converting a workload into CrashMonkey test case ###
 Let us write a CrashMonkey test case for the following simple workload:
 
-```c++
+```
 setup:
       mkdir A
-      fsync A
+      create A/foo
+      sync
 
 run:
-      create A/foo
-      write A/foo <some content>
+      mkdir B
+      link A/foo B/foo
       fsync A/foo
       **Mark this point**
 
 check:
-      Does A/foo exist and have <some content> if you crash after the **Mark**?
+      Do A/foo and B/foo exist if you crash after the **Mark**?
 ```
+
+
+#### Setup ####
+
+```c++
+virtual int setup() override {
+
+  // initialize paths.
+  // mnt_dir_ points to the root directory by default
+    A_path = mnt_dir_ + "/A";
+    Afoo_path =  mnt_dir_ + "/A/foo";
+
+    // create the directory
+    if ( mkdir(A_path.c_str() , 0777) < 0 ){
+        return errno;
+    }
+
+    // create a file
+    int fd_Afoo = cm_->CmOpen(Afoo_path.c_str() , O_RDWR|O_CREAT , 0777);
+    if ( fd_Afoo < 0 ) {
+      cm_->CmClose( fd_Afoo);
+      return errno;
+    }
+
+    cm_->CmSync();
+
+    //close open files
+    if ( cm_->CmClose ( fd_Afoo ) < 0 ) {
+        return errno;
+    }
+    return 0;
+}
+```
+
+So, you would have noticed that the only difference between this and a normal C++ code is the wrapper function around system calls. CrashMonkey profiles the workload to be able to track the list of files and directories being persisted by the workload. This information is necessary to enable automatic testing.
+
+#### Run ####
+
+Similar to the setup phase, you can write the corresponding system calls in the run method. The important aspect of run method is **Checkpoint**. CrashMonkey defines an optional Checkpoint flag to be inserted in the block IO sequence to indicate the completion of some operation. For example, in this workload, we should be able to check if `B/foo` is correctly recovered if a crash happened after the `fsync(A/foo)`. The auto-checker compulsorily requires that any persistence operation be followed by a checkpoint, because crashes are simulated only at these points. Let's see how to insert checkpoint in the run method.
+
+```c++
+virtual int run( int checkpoint ) override {
+
+  int local_checkpoint = 0 ;
+
+  < some workload >
+
+  < PERSISTENCE OP 1 >
+
+  if ( cm_->CmCheckpoint() < 0){
+      return -1;
+  }
+  local_checkpoint += 1;
+  if (local_checkpoint == checkpoint) {
+      return 0;
+
+  < continue workload >
+
+  < LAST PERSISTENCE OP >
+
+  if ( cm_->CmCheckpoint() < 0){
+      return -1;
+  }
+  local_checkpoint += 1;
+  if (local_checkpoint == checkpoint) {
+      return 1;
+
+  return 0;
+}
+```
+
+CrashMonkey snapshots disk images at each checkpoint to create oracle for testing. Therefore, it is necessary to indicate the last checkpoint in the workload. To do so, all the checkpoints in the workload except the last one, returns 0. The last checkpoint must return 1.
+
+#### Check_Test ####
+
+For the above workload, we'll perform two tests.
+1. Check if A/foo is present, no matter where the crash occurs, because we persisted it in the setup phase.
+2. Check if the link B/foo is present, if we crashed after persisting A/foo.
+
+Again, there is no special instruction on writing these checks. Write it as you'd do for any C++ file. However the interesting aspect here is how to communicate the bug type and a status string to the test harness. You can do this by setting the error status to one of the pre-defined types : `kFileMissing`, `kFileDataCorrupted`, etc. The complete list is available [here](../results/DataTestResult.cpp). Additionally, you can also provide a short description about the failure.
+
+```c++
+virtual int check_test( unsigned int last_checkpoint, DataTestResult *test_result) override {
+
+  struct stat stats;
+  int res_Bfoo = stat(Bfoo_path.c_str(), &stats);
+  if ( last_checkpoint == 1 && res_Bfoo < 0 ) {
+      test_result->SetError(DataTestResult::kFileMissing);
+      test_result->error_description = " : Missing file " + Bfoo_path.c_str();
+      return 0;
+  }
+  return 0;
+}
+```
+
+___
+
+Now that completes our test case! [See the complete test case](../test/example.cpp).
+
+You can run this workload either with auto-checker (use `-c` flag), or by skipping it and invoking only the consistency checks we defined. In either case, you should see similar results.
